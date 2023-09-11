139282fa04aa996b5e0c52d73fd18032429d0229
Загуменнов Константин <zagumennov_ka@directum.ru>
2023-09-11 13:35:13 +0400

    Add class PhoheNumber

---
+++ PhoneBook_CRUD/PhoneNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook_CRUD
+{
+    internal class PhoneNumber
+    {
+
+        private int number = 1234567890;
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+    }
+}
