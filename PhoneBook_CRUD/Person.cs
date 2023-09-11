ab1ff42fcded4d9494715c4597ffb919a3150010
Загуменнов Константин <zagumennov_ka@directum.ru>
2023-09-11 13:28:34 +0400

    Add class Person
    Delete wrong cashe in repo. Add new project and add new class Person.

---
+++ PhoneBook_CRUD/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook_CRUD
+{
+    internal class Person
+    {
+        private string name = "Безымянный";
+        
+        public string Name 
+        { 
+            get
+            {
+
+                return name;
+
+            }
+            set 
+            {
+
+                name = value;
+
+            }
+        }
+    }
+}
